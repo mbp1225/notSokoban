68f9f454e558c7a634f3b150a59f4311b821a654
mbp1225 <matheus698@hotmail.com>
2017-08-22 16:01:34 -0300

    The buttons work. The cube can still enter walls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject commandsHelp;
+
+    void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.H))
+        {
+            commandsHelp.SetActive(!commandsHelp.activeInHierarchy);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
